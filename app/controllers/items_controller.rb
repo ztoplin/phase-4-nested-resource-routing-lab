93f3915f429aca00a90f2f90d500165f42c99b44
Aaron <ztoplin@yahoo.com>
2022-12-18 19:45:41 -0500

    commit tree 6f08de3106bb58a86488f43736148f05bb3c5e5c

--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -1,8 +1,47 @@
 class ItemsController < ApplicationController
+  rescue_from ActiveRecord::RecordNotFound, with: :render_not_found_response
 
   def index
-    items = Item.all
-    render json: items, include: :user
+
+   if params[:user_id]
+      user = find_user 
+      items = user.items 
+   else
+      items = Item.all
+   end
+
+   render json: items, include: :user
+
+  end
+
+  def show 
+    item = find_item 
+    render json: item
+  end
+
+  def create 
+    user = find_user 
+    item = user.items.create(item_params)
+    render json: item, status: :created
+  end
+
+
+  private 
+
+  def find_item 
+    Item.find(params[:id])
+  end
+
+  def find_user 
+    User.find(params[:user_id])
+  end
+
+  def item_params
+    params.permit(:name, :description, :price)
+  end
+
+  def render_not_found_response(exception)
+    render json: { error: "#{exception.model} not found"}, status: :not_found
   end
 
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,10 @@
 Rails.application.routes.draw do
+
   resources :items, only: [:index]
-  resources :users, only: [:show]
+
+  resources :users, only: [:show] do
+    # nested resource for items 
+    resources :items, only: [:show, :index, :create]
+  end
+
 end
